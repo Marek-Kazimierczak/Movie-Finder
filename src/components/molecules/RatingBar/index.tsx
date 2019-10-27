db59e2a30c5bd196d4abbf098d3b08c702dbe763
Marek Kazimierczak <mrmkazimierczak@gmail.com>
2019-10-27 10:40:23 +0100

    Update rating component

--- src/components/molecules/RatingBar/index.tsx
+++ src/components/molecules/RatingBar/index.tsx
@@ -4,7 +4,6 @@ import RatingStar from "../../atoms/RatingStar";
 
 import "./RatingBar.scss";
 
-//movie.vote_average
 const RatingBar = () => {
   const [activeRate, setActiveRate] = useState(0);
   const [halfRate, setHalfRate] = useState(false);
@@ -20,25 +19,20 @@ const RatingBar = () => {
         ? "half-star"
         : "empty-star";
     });
-
     setRating(values);
   };
 
   useEffect(() => {
-    if (movie) {
-      setActiveRate(Math.round(movie.vote_average));
+    setActiveRate(Math.floor(movie.vote_average));
 
-      const isHalfRatingUnit =
-        ((Math.ceil(movie.vote_average * 2) / 2).toFixed(1) as any) % 1 === 0.5;
+    const isHalfRatingUnit =
+      ((Math.ceil(movie.vote_average * 2) / 2).toFixed(1) as any) % 1 === 0.5;
 
-      isHalfRatingUnit ? setHalfRate(true) : setHalfRate(false);
-    }
-  }, []);
+    isHalfRatingUnit ? setHalfRate(true) : setHalfRate(false);
+  }, [movie]);
 
   useEffect(() => {
-    if (movie) {
-      generateRatingValues();
-    }
+    generateRatingValues();
   }, [activeRate]);
 
   const ratingUnits = rating.map((rate: string, index) => (
